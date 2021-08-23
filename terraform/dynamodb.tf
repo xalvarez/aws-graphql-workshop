3c34bc1e76d00c0edb5b5c937460f41c4a35a232
Xavier Alvarez <xalvarez@users.noreply.github.com>
2021-08-23 17:53:58 +0200

    Add terraform script

---
+++ terraform/dynamodb.tf
@@ -0,0 +1,75 @@
+resource "aws_dynamodb_table" "companies_table" {
+  name           = "Companies"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "brands_table" {
+  name           = "Brands"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "products_table" {
+  name           = "Products"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_iam_role" "role" {
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "appsync.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "role_policy" {
+  role = aws_iam_role.role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "dynamodb:*"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_dynamodb_table.companies_table.arn}",
+        "${aws_dynamodb_table.brands_table.arn}",
+        "${aws_dynamodb_table.products_table.arn}"
+      ]
+    }
+  ]
+}
+EOF
+}
